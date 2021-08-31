12ffe53aeb7cdabaf61182a8180db5e6823fd476
Srutip04 <subuhishruti75@gmail.com>
2021-09-01 01:41:58 +0530

    structure of node declared

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+struct node
+{
+    /* data */
+    char name[50];//name of contact
+    char num[10];//phone number of contact
+    struct node *next; //next pointer to store the address of next node
+    
+};
+
+typedef struct node Node;
+typedef struct node *link;
+
+
+
+
+
